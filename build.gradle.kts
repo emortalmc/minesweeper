efa78aed62f60bc04202172b0cd4090654b59f9d
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2023-06-19 19:49:14 +0100

    Update minestom core

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 dependencies {
     implementation("com.google.protobuf:protobuf-java-util:3.21.12")
 
-    implementation("dev.emortal.minestom:core:63a461b")
+    implementation("dev.emortal.minestom:core:85ae46e")
     implementation("dev.emortal.minestom:game-sdk:4d22719")
 
 //    implementation("net.kyori:adventure-text-minimessage:4.13.0")
