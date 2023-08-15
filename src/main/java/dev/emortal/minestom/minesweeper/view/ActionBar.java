6f984188863a2c68d994106cf70570b8d389319e
emortaldev <me@emortal.dev>
2023-08-15 04:09:02 +0100

    Add timer and make actionbar always visible

--- src/main/java/dev/emortal/minestom/minesweeper/view/ActionBar.java
+++ src/main/java/dev/emortal/minestom/minesweeper/view/ActionBar.java
@@ -3,18 +3,26 @@ package dev.emortal.minestom.minesweeper.view;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.minestom.server.instance.Instance;
+import net.minestom.server.timer.TaskSchedule;
 import org.jetbrains.annotations.NotNull;
+
+import java.time.Duration;
 
 public final class ActionBar {
 
     private final Instance instance;
-
     private final int mines;
     private int flags;
+    private final long startTime;
 
     public ActionBar(@NotNull Instance instance, int mines) {
         this.instance = instance;
         this.mines = mines;
+
+        this.startTime = System.currentTimeMillis();
+
+        // Keep action bar shown
+        instance.scheduler().buildTask(this::update).repeat(TaskSchedule.tick(20)).schedule();
     }
 
     public void incrementFlags() {
@@ -23,14 +31,33 @@ public final class ActionBar {
     }
 
     public void update() {
-        // ☠ {mines} MINES | ⚑ {flags} FLAGS
-        instance.sendActionBar(Component.text()
-                .append(Component.text("☠ ", NamedTextColor.RED))
-                .append(Component.text(mines, NamedTextColor.RED))
-                .append(Component.text(" MINES", NamedTextColor.RED))
-                .append(Component.text(" | ", NamedTextColor.DARK_GRAY))
-                .append(Component.text("⚑ ", NamedTextColor.GREEN))
-                .append(Component.text(flags, NamedTextColor.GREEN))
-                .append(Component.text(" FLAGS", NamedTextColor.GREEN)));
+        Duration duration = Duration.ofMillis(System.currentTimeMillis() - startTime);
+
+        // ☠ {mines} MINES | ⚑ {flags} FLAGS | ⌚ 1m 23s
+        instance.sendActionBar(
+                Component.text()
+                        .append(Component.text("☠ ", NamedTextColor.RED))
+                        .append(Component.text(mines, NamedTextColor.RED))
+                        .append(Component.text(" MINES", NamedTextColor.RED))
+                        .append(Component.text(" | ", NamedTextColor.DARK_GRAY))
+                        .append(Component.text("⚑ ", NamedTextColor.GREEN))
+                        .append(Component.text(flags, NamedTextColor.GREEN))
+                        .append(Component.text(" FLAGS", NamedTextColor.GREEN))
+                        .append(Component.text(" | ", NamedTextColor.DARK_GRAY))
+                        .append(Component.text("⌚ ", NamedTextColor.AQUA))
+                        .append(Component.text(formatDuration(duration), NamedTextColor.AQUA))
+        );
+    }
+
+    private String formatDuration(Duration duration) {
+        long hours = duration.toHours();
+        long mins = duration.toMinutesPart();
+        long secs = duration.toSecondsPart();
+
+        if (hours == 0) {
+            return String.format("%dm %ds", mins, secs);
+        } else {
+            return String.format("%dh %dm %ds", hours, mins, secs);
+        }
     }
 }
