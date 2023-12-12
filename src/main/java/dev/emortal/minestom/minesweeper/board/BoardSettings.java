4818bbcf7ecfd65218a67f473d7bca1755262a02
Callum Seabrook <callum.seabrook@prevarinite.com>
2023-12-12 22:41:53 +0000

    Set default mines back to 170

--- src/main/java/dev/emortal/minestom/minesweeper/board/BoardSettings.java
+++ src/main/java/dev/emortal/minestom/minesweeper/board/BoardSettings.java
@@ -3,5 +3,5 @@ package dev.emortal.minestom.minesweeper.board;
 import org.jetbrains.annotations.NotNull;
 
 public record BoardSettings(int length, int width, int mines) {
-    public static final @NotNull BoardSettings DEFAULT = new BoardSettings(30, 30, 10);
+    public static final @NotNull BoardSettings DEFAULT = new BoardSettings(30, 30, 170);
 }
