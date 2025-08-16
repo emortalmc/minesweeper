45a28c104629f8433262ab9c8e25bd4022d935c6
Thyamix <random@thyamix.com>
2025-08-16 23:48:41 +0200

    fix: replaced green player with cyan

--- src/main/java/dev/emortal/minestom/minesweeper/util/TeamColor.java
+++ src/main/java/dev/emortal/minestom/minesweeper/util/TeamColor.java
@@ -6,36 +6,36 @@ import org.jetbrains.annotations.NotNull;
 
 public enum TeamColor {
 
-    RED(NamedTextColor.RED, Block.RED_CARPET),
-    ORANGE(NamedTextColor.GOLD, Block.ORANGE_CARPET),
-    GREEN(NamedTextColor.DARK_GREEN, Block.GREEN_CARPET),
-    BLUE(NamedTextColor.BLUE, Block.BLUE_CARPET),
-    PINK(NamedTextColor.LIGHT_PURPLE, Block.PINK_CARPET),
-    PURPLE(NamedTextColor.DARK_PURPLE, Block.PURPLE_CARPET),
-    AQUA(NamedTextColor.AQUA, Block.CYAN_CARPET),
-    YELLOW(NamedTextColor.YELLOW, Block.YELLOW_CARPET),
-    DARK_AQUA(NamedTextColor.DARK_AQUA, Block.CYAN_CARPET),
-    LIGHT_GRAY(NamedTextColor.GRAY, Block.LIGHT_GRAY_CARPET),;
+                                RED(NamedTextColor.RED, Block.RED_CARPET),
+                                ORANGE(NamedTextColor.GOLD, Block.ORANGE_CARPET),
+                                CYAN(NamedTextColor.DARK_AQUA, Block.CYAN_CARPET),
+                                BLUE(NamedTextColor.BLUE, Block.BLUE_CARPET),
+                                PINK(NamedTextColor.LIGHT_PURPLE, Block.PINK_CARPET),
+                                PURPLE(NamedTextColor.DARK_PURPLE, Block.PURPLE_CARPET),
+                                AQUA(NamedTextColor.AQUA, Block.CYAN_CARPET),
+                                YELLOW(NamedTextColor.YELLOW, Block.YELLOW_CARPET),
+                                DARK_AQUA(NamedTextColor.DARK_AQUA, Block.CYAN_CARPET),
+                                LIGHT_GRAY(NamedTextColor.GRAY, Block.LIGHT_GRAY_CARPET),;
 
-    private static final TeamColor[] VALUES = values();
+                                private static final TeamColor[] VALUES = values();
 
-    public static @NotNull TeamColor fromId(byte id) {
-        return VALUES[id];
-    }
+                                public static @NotNull TeamColor fromId(byte id) {
+                                                                return VALUES[id];
+                                }
 
-    private final @NotNull NamedTextColor color;
-    private final @NotNull Block carpet;
+                                private final @NotNull NamedTextColor color;
+                                private final @NotNull Block carpet;
 
-    TeamColor(@NotNull NamedTextColor color, @NotNull Block carpet) {
-        this.color = color;
-        this.carpet = carpet;
-    }
+                                TeamColor(@NotNull NamedTextColor color, @NotNull Block carpet) {
+                                                                this.color = color;
+                                                                this.carpet = carpet;
+                                }
 
-    public @NotNull NamedTextColor color() {
-        return this.color;
-    }
+                                public @NotNull NamedTextColor color() {
+                                                                return this.color;
+                                }
 
-    public @NotNull Block carpet() {
-        return this.carpet;
-    }
+                                public @NotNull Block carpet() {
+                                                                return this.carpet;
+                                }
 }
